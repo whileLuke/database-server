4f616f2bab317fed92b9aaa82424132f867496ef
whileLuke <pm24104@bristol.ac.uk>
2025-03-12 18:23:47 +0000

    Refactoring Updates

--- src/main/java/edu/uob/NotAllowedWords.java
+++ src/main/java/edu/uob/NotAllowedWords.java
@@ -1,21 +1,15 @@
 package edu.uob;
 
 import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.List;
 
 public class NotAllowedWords {
-    private static final Set<String> RESERVED_WORDS = new HashSet<>(Arrays.asList(
-            "USE", "CREATE", "DATABASE", "TABLE", "DROP", "ALTER", "INSERT",
-            "INTO", "VALUES", "DELETE", "FROM", "UPDATE", "SET", "SELECT",
-            "JOIN", "ON", "WHERE", "AND", "OR", "NOT", "NULL", "TRUE", "FALSE"
-    ));
+    private static final List<String> notAllowedWords = Arrays.asList(
+            "USE", "CREATE", "DATABASE", "TABLE", "DROP",
+            "ALTER", "INSERT", "INTO", "VALUES", "DELETE",
+            "FROM", "UPDATE", "SET", "SELECT", "JOIN", "ON",
+            "WHERE", "AND", "OR", "NULL", "TRUE", "FALSE"
+    );
 
-    public static boolean isNotAllowed(String word) {
-        return RESERVED_WORDS.contains(word.toUpperCase());
-    }
-
-    public static boolean isAllowed(String name) {
-        return !isNotAllowed(name);
-    }
+    public static boolean isNotAllowed(String word) { return notAllowedWords.contains(word.toUpperCase()); }
 }
